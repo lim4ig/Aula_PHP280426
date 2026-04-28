bb05b7421a52e686343bd3bb06328de8f23bef33
lima <igorrmatheus2005@gmail.com>
2026-04-28 20:12:17 -0300

    Começo

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>História da Programação</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h4>História da programação</h4>
+    <h1>Mulheres que fizeram a diferença</h1>
+    <hr>
+    <?php
+    // aqui criar o array de mulheres
+    $mulheres = [
+
+    ];
+    ?>
+
+    <div class="galeria">
+        
+        <?php /*cada item é o conjunto de informações de uma mulher*/  ?>
+        <div class="item">
+            
+        </div>
+        
+    </div>
+
+    
+</body>
+</html> 
